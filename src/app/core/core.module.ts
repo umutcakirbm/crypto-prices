6db067ef312b9b536903f15872ea34cc86842431
Umut ÇAKIR <umutcakirbm@gmail.com>
2022-06-24 00:09:13 +0300

    move core components to components folder

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { CoreRoutingModule } from './core-routing.module';
 import { httpInterceptorProviders } from './interceptors';
 
