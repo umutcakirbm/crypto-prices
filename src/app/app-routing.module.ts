cf7f22c2568f7b58a91ba370cb1ce4117333736d
Umut ÇAKIR <umutcakirbm@gmail.com>
2022-06-23 18:36:36 +0300

    create page and route structure

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {
+    path: 'crypto',
+    loadChildren: () =>
+      import('./crypto/crypto.module').then((m) => m.CryptoModule),
+  },
+  { path: '', redirectTo: '/crypto', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/crypto/crypto.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CryptoRoutingModule } from './crypto-routing.module';
+import { ViewComponent } from './view/view.component';
+
+
+@NgModule({
+  declarations: [
+    ViewComponent
+  ],
+  imports: [
+    CommonModule,
+    CryptoRoutingModule
+  ]
+})
+export class CryptoModule { }
